e9aebc80ca263830dfa95a608b6f0f36b713a4eb
Ruaidhri O'Neill <Ruaidhri2006@gmail.com>
2021-07-08 14:05:17 +0100

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Ruaidhrí O'Neill on 08/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
